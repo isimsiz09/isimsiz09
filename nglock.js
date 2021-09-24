8750b4425845268b787bce5010bf33eae8812ea4
isimsiz09 <91325048+isimsiz09@users.noreply.github.com>
2021-09-24 17:48:49 +0500

    Update nglock.js

--- nglock.js
+++ nglock.js
@@ -1,13 +1,5 @@
 <script>
  alert();
 
-// function lockSite(arg)
-// {
-//  alert();
-// if(arg == "yes")
-// {
-// document.getElementByTagName("body").style.display = "none";
-// }
-// }
-// lockSite("yes")
+
 </script>
